7c6656ea728ad839eede849772136a57c5fcc9f4
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-17 17:22:42 -0600

    oof

--- python/sample.py
+++ python/sample.py
@@ -1,9 +1,13 @@
 import vim
+import re
 # print vim.eval('expand(\'%:p\')')
 
 def GetSyntaxHighlighting():
     filePath = vim.eval('expand(\'%:p\')')
     fileBuffer = open(filePath, "r");
-    print fileBuffer.read()
+    structList = []
+    for line in fileBuffer:
+        txt = re.findall("^\s*typedef\s+(struct|union|enum)\s+\w+", line)
+        print txt
 
 GetSyntaxHighlighting()
