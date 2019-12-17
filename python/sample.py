800756faa3aa0fec4267beaa7e9697200b72057a
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-17 17:03:25 -0600

    oof

--- python/sample.py
+++ python/sample.py
@@ -1,2 +1,9 @@
 import vim
-print vim.eval('expand(\'%:p\')')
+# print vim.eval('expand(\'%:p\')')
+
+def GetSyntaxHighlighting:
+    filePath = vim.eval('expand(\'%:p\')')
+    fileBuffer = open(filepath, "r");
+    print fileBuffer.read()
+
+GetSyntaxHighlighting()
