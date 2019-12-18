dcb8a22325ab4fcb2c0966eb31e14d91c9daaf71
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-18 11:04:16 -0600

    oof

--- rplugin/python/sample.py
+++ rplugin/python/sample.py
@@ -5,17 +5,24 @@ class Main(object):
 
     def __init__(self, vim):
         self.vim = vim
+    
     @neovim.function('GetList')
-    def getList(self, args):
+    def getFiles(self, args):
         filePath = self.vim.eval('expand(\'%:p\')')
         fileBuffer = open(filePath, "r");
-        structList = []
+        fileList = []
+        for line in fileBuffer:
+            txt = re.search("^#include\+\"([\w.]+)\"")
+            if txt != None:
+                self.vim.command("echo \"" + txt.group(1) + "\"")
+
+    def getList(self, files):
         for line in fileBuffer:
             txt = re.search("^\s*typedef\s+struct\s+(\w+)\s*{*", line)
             if txt != None:
-                structList.append(txt.group(1))
-        self.vim.command("echo \"" + ' '.join(structList) + "\"")
-        
+                structList.append(txt.group(1)) 
+
+    def HighLightSyntax(self, structList):
         if len(structList) > 0:
             self.vim.command("syntax keyword Custom_Python_Identifiers " + ' '.join(structList))
             self.vim.command("highlight link Custom_Python_Identifiers Identifier")
