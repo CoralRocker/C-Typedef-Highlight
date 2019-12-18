33b697e941a6eafde726cac80bbfcd00fc4a5620
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-18 10:06:50 -0600

    oof

--- rplugin/python/sample.py
+++ rplugin/python/sample.py
@@ -7,10 +7,11 @@ class Main(object):
         self.vim = vim
     @neovim.function('GetList')
     def getList(self, args):
-        #filePath = vim.eval('expand(\'%:p\')')
-        #fileBuffer = open(filePath, "r");
-        #structList = []
-        #for line in fileBuffer:
-        #    txt = re.findall("^\s*typedef\s+(struct|union|enum)\s+\w+", line)
-        #    print txt
-        self.vim.command('echo "Hello"')
+        filePath = vim.eval('expand(\'%:p\')')
+        fileBuffer = open(filePath, "r");
+        structList = []
+        for line in fileBuffer:
+            txt = re.findall("^\s*typedef\s+(struct|union|enum)\s+\w+", line)
+           # print txt
+            self.vim.command(f'echo {txt}')
+
