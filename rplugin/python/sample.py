d1dee0519477d3972c290339c4728fbf9ae38e01
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-18 09:59:37 -0600

    IDK

---
+++ rplugin/python/sample.py
@@ -0,0 +1,16 @@
+import neovim
+
+@neovim.plugin
+class Main(object):
+
+    def __init__(self, vim):
+        self.vim = vim
+    @neovim.function('GetList')
+    def getList(self, args):
+        filePath = vim.eval('expand(\'%:p\')')
+        fileBuffer = open(filePath, "r");
+        structList = []
+        for line in fileBuffer:
+            txt = re.findall("^\s*typedef\s+(struct|union|enum)\s+\w+", line)
+            print txt
+
