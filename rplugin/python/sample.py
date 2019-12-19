e14a77fa8058f2407796eec6c72c6dfb2908f68b
Gaultier Delbarre <gaultiertexas@gmail.com>
2019-12-19 08:45:28 -0600

    oof

--- rplugin/python/sample.py
+++ rplugin/python/sample.py
@@ -23,7 +23,7 @@ class Main(object):
             f.close()
         self.HighLightSyntax(structList)
 
-    @neovim.command('ReSyn', range='', nargs=0, sync=True)
+    @neovim.command('GetSyntaxHighlighting')
     def getFiles(self, args):
         filePath = self.vim.eval('expand(\'%:p\')')
         fileBuffer = open(filePath, "r");
